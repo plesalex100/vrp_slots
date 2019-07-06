38e802978ef8e651d56e3505fc1090686e966033
Ples Alex <34649178+plesalex100@users.noreply.github.com>
2019-07-06 09:38:56 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,6 +1,6 @@
--- Aceasta resursa a fost facuta de plesalex100#7387
--- Va rog sa o respectati, sa nu o vinde-ti sau postati fara permisiunea mea
--- Aceasta resursa a pornit de la: https://codepen.io/AdrianSandu/pen/MyBQYz
+-- This resource was made by plesalex100#7387
+-- Please respect it, don't repost it without my permission
+-- This Resource started from: https://codepen.io/AdrianSandu/pen/MyBQYz
 
 
 local open = false
@@ -13,7 +13,7 @@ end
 
 Citizen.CreateThread(function()
 	if Cfg.blipsEnabled then
-	  for k,v in ipairs(Cfg.Pacanele)do
+		for k,v in ipairs(Cfg.Pacanele)do
 			local blip = AddBlipForCoord(v.x, v.y, v.z)
 			SetBlipSprite(blip, 436)
 			SetBlipDisplay(blip, 4)
@@ -23,41 +23,41 @@ Citizen.CreateThread(function()
 			BeginTextCommandSetBlipName("STRING")
 			AddTextComponentString("Pacanele")
 			EndTextCommandSetBlipName(blip)
-	  end
+		end
 	end
 end)
 
 Citizen.CreateThread(function ()
-  SetNuiFocus(false, false)
+	SetNuiFocus(false, false)
 	open = false
 
 	local wTime = 500
 	local x = 1
-  while true do
-    Citizen.Wait(wTime)
+	while true do
+		Citizen.Wait(wTime)
 		langaAparat = false
 
-    for i=1, #Cfg.Pacanele, 1 do
-      if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), Cfg.Pacanele[i].x, Cfg.Pacanele[i].y, Cfg.Pacanele[i].z, true) < 2  then
+		for i=1, #Cfg.Pacanele, 1 do
+			if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), Cfg.Pacanele[i].x, Cfg.Pacanele[i].y, Cfg.Pacanele[i].z, true) < 2  then
 				x = i
 				wTime = 0
 				langaAparat = true
-				drawHint('Apasa ~INPUT_PICKUP~ pentru a-ti incerca norocul la pacanele')
+				drawHint('Press ~INPUT_PICKUP~ to test your luck at slot machine')
 			elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), Cfg.Pacanele[x].x, Cfg.Pacanele[x].y, Cfg.Pacanele[x].z, true) > 4 then
 				wTime = 500
 			end
-    end
+		end
 	end
 end)
 
 RegisterNetEvent("ples-slots:bagXLei")
 AddEventHandler("ples-slots:bagXLei", function(lei)
-  SetNuiFocus(true, true)
-  open = true
-  SendNUIMessage({
-    showPacanele = "open",
-    coinAmount = tonumber(lei)
-  })
+	SetNuiFocus(true, true)
+	open = true
+	SendNUIMessage({
+		showPacanele = "open",
+		coinAmount = tonumber(lei)
+	})
 end)
 
 RegisterNUICallback('exitWith', function(data, cb)
@@ -68,16 +68,16 @@ RegisterNUICallback('exitWith', function(data, cb)
 end)
 
 Citizen.CreateThread(function ()
-  while true do
-    Citizen.Wait(1)
+	while true do
+		Citizen.Wait(1)
 		if open then
-      DisableControlAction(0, 1, true) -- LookLeftRight
-      DisableControlAction(0, 2, true) -- LookUpDown
-      DisableControlAction(0, 24, true) -- Attack
-      DisablePlayerFiring(GetPlayerPed(-1), true) -- Disable weapon firing
-      DisableControlAction(0, 142, true) -- MeleeAttackAlternate
-      DisableControlAction(0, 106, true) -- VehicleMouseControlOverride
-    elseif IsControlJustReleased(0, 38) and langaAparat then
+			DisableControlAction(0, 1, true) -- LookLeftRight
+			DisableControlAction(0, 2, true) -- LookUpDown
+			DisableControlAction(0, 24, true) -- Attack
+			DisablePlayerFiring(GetPlayerPed(-1), true) -- Disable weapon firing
+			DisableControlAction(0, 142, true) -- MeleeAttackAlternate
+			DisableControlAction(0, 106, true) -- VehicleMouseControlOverride
+		elseif IsControlJustReleased(0, 38) and langaAparat then
 			TriggerServerEvent('ples-slots:catiLeiBagi')
 		end
 	end
